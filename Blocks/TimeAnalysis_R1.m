3a6794674445ca7210519d09f35c68b59c9208b4
Risky Jr <2114768901@qq.com>
2019-07-31 00:46:57 +0800

    单日有效时间统计完成

--- Blocks/TimeAnalysis_R1.m
+++ Blocks/TimeAnalysis_R1.m
@@ -11,7 +11,9 @@ fprintf("load path:%s\n",pwd);
 
 %}
 clc;clear;
+close all;
 disp('Run time table script')
+Time_01 = tic;
 timetable = 'C:\Users\10520\Desktop\TimeItem.txt';
 table_origin = readtable(timetable,'Delimiter','|',...
     'FileEncoding','utf-8','ReadVariableNames',false...
@@ -27,7 +29,6 @@ for i = 1:1:valid_table_size(1)
     for k = 1:1:valid_table_size(2)
         chartemp = char(valid_table{i,k});
         emptylineflag = emptylineflag & isempty(chartemp);
-
     end
     if emptylineflag
         emptyline = i-1;
@@ -56,12 +57,12 @@ clear chartemp emptyline emptylineflag i k size_temp timetable ...
     table_origin valid_table_size
 
 
-%--------------------nothing ----------------%
+%--------------------valid struct ----------------%
 heading = {'date','time','item','other'};
 % todo:@parameter 2 ???? 
 valid_struct = cell2struct(valid_cell,heading,2);
 structSizeTemp = size(valid_struct);
-clc;
+% clc;
 disp('valid datas input')
 
 for i = 1:1:structSizeTemp(1)
@@ -77,7 +78,7 @@ for i = 1:1:structSizeTemp(1)
                       datevec(startTimeTemp,'yyyy/mm/dd HH:MM'));
     if interTemp < 0
         warning('...');
-        fprintf('rows-> (%d) time calculation may wrong->(%d)\n',i,interTemp);
+        % fprintf('rows-> (%d) time calculation may wrong->(%d)\n',i,interTemp);
         interTemp = abs(interTemp);
     end
     valid_struct(i).interval = int32(interTemp/60);  % min
@@ -98,7 +99,8 @@ for i = 1:1:structSizeTemp(1)
     end
     valid_struct(i).mand = mandtemp;
 end
-
+disp('Valid Struct Got')
+toc(Time_01)
 % clear data_temp finishTimeTemp heading MAND len_temp i mandtemp;
 % clear rep structSizeTemp startTimeTemp interTemp timetemp
 
@@ -109,6 +111,7 @@ end
     设计思路：
     1.初步统计事项，时间
 %}
+disp('统计分析...')
 anlysis_1 = true;
 if anlysis_1
     periods = zeros(0);
@@ -121,11 +124,11 @@ if anlysis_1
     end
     explode = logical(periods);
     pie_temp = double(periods);
-    if false
-        figure(1)
+    if true
+        disp('绘制粗略图')
+        figure('name','概图')
         subplot(1,2,1)
-        suptitle("???????");
-        % plot(pie_temp)
+        suptitle('概图');
         if true
             bar(pie_temp,'FaceColor','c','EdgeColor',[0 .5 .5],...
             'LineWidth',1.5)
@@ -134,14 +137,13 @@ if anlysis_1
             'LineWidth',1.5)
         end
         grid on
-
         subplot(1,2,2)
         pie(pie_temp,explode,pie_labels);
         grid on
     end
 end
-
-%% 
+toc(Time_01)
+%%
 %--------------统计分析-------------%
 %{
    设计思路：
@@ -150,7 +152,7 @@ end
    3.大类时间，具体事项时间；
    4.数据存储格式选择cell；
 %}
-clc;
+% clc;
 board = strings(0);
 detail = strings(0);
 board_temp = "";
@@ -165,12 +167,6 @@ if anlysis_items
     % 天数统计
     
     for i = linspace(1,len_temp(1),len_temp(1))
-        % 天数统计
-        % if ~contains(days_temp,valid_struct(i).date)
-        %     days_temp = days_temp + " " + valid_struct(i).date;
-        %     daynum = daynum + 1 ;
-        % end
-
         periods_temp = valid_struct(i).interval;
         thing_temp = string(valid_struct(i).item);
         if ~contains(board_temp,thing_temp)
@@ -187,13 +183,80 @@ if anlysis_items
         board(strlength(board) < 1) = [];
         % 云图
         if true
+            disp('绘制云图')
+            figure('name','云图')
             draw_words = categorical(board);
             wordcloud(draw_words,linspace(1,length(draw_words),length(draw_words)));
         end
     end
 end
-
-%
 % clear explode i len_temp periods pie_labels pie_temp anlysis_1
 %% 
-% day_1 = valid_struct(end).date - valid_struct(1).date;
+%---------统计一天有效时间-----------%
+%{
+    设计思路：
+    1.nestedstruct结构存储；
+    2.统计一天有效时间；
+%}
+% clc;
+disp('统计一天有效时间')
+% hours
+days = hours(datetime(valid_struct(end).date) ...
+             - datetime(valid_struct(1).date));
+days = days/24;
+fprintf('All days:%d\n',days);
+
+valid_day_neststruct = struct('date',{},'validPeriod',{},'item',...
+                             struct('detail',{},'detailPeriod',{}));
+
+% 日期在struct中的位置
+datePosTemp = zeros(0);
+nestStructCnt = 1;
+dateStrTemp = "";
+for i=1:1:length(valid_struct)
+    % nestStruct(n).date
+    if ~isequal(dateStrTemp, datestr(datetime(valid_struct(i).date),'yyyy-mm-dd'))
+        dateStrTemp = datestr(datetime(valid_struct(i).date),'yyyy-mm-dd');
+        valid_day_neststruct(nestStructCnt).date = dateStrTemp;
+        datePosTemp(nestStructCnt) = i;
+        nestStructCnt = nestStructCnt + 1;
+    end
+    if i == length(valid_struct)
+        datePosTemp(nestStructCnt) = length(valid_struct);
+    end
+end
+
+% 每天有效时间统计 min
+nestStructCnt = 1;
+for i = 1:1:length(datePosTemp)-1
+    valid_day_neststruct(nestStructCnt).validPeriod = 0;
+    try
+        for k = linspace(datePosTemp(i),datePosTemp(i+1),datePosTemp(i+1) - datePosTemp(i)+1)
+            valid_day_neststruct(nestStructCnt).validPeriod =...
+             valid_day_neststruct(nestStructCnt).validPeriod...
+             + valid_struct(k).interval;
+        end
+    catch
+        fprintf('索引错误%d\n',k);
+    end
+    nestStructCnt = nestStructCnt + 1;
+end
+% 绘制柱状图
+if true
+    datePeriod = zeros(0);
+    datePeriodDate = strings(0);
+    for i=1:1:length(valid_day_neststruct)
+        datePeriod(i) = valid_day_neststruct(i).validPeriod/60;
+        datePeriodDate(i) = string(valid_day_neststruct(i).date);
+        cg_datePeriodDate = categorical(datePeriodDate);
+    end 
+    pic_name01 = 'draw day vliad period';
+    disp(pic_name01)
+    figure('name','有效时间柱状图')
+    bar(cg_datePeriodDate,datePeriod,'FaceColor',[0 .5 .5],...
+        'EdgeColor',[0 .9 .9],'LineWidth',1.5)
+    set(get(gca, 'Title'), 'String', pic_name01);
+    set(get(gca, 'XLabel'), 'String', '小时');
+    grid on
+end
+toc(Time_01)
