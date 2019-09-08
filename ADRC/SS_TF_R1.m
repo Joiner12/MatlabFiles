b3aa3b3e107ea96701c7f4b9d361d8c8aeac9590
Risky Jr <2114768901@qq.com>
2019-09-08 23:18:07 +0800

    系统建模

--- ADRC/SS_TF_R1.m
+++ ADRC/SS_TF_R1.m
@@ -14,4 +14,23 @@ T = 1e-3;
 s = tf('s');
 tf_ob = 16/(s^2 + 0.5*s)
 sys = ss(tf_ob);
-[num,den] = tfdata(c2d(tf_ob,T,'zoh'),'v')
+[num,den] = tfdata(c2d(tf_ob,T,'tustin'),'v');
+
+figure(1)
+step(feedback(sys,1))
+
+%% 
+%{
+    系统模型
+    Gp(s) = 0.00011167(s + 33006)/(s^2 + 1.245s)
+    reference:
+    [1]王永强，张承瑞，滚珠丝杠进给系统仿真建模，振动与冲击，2013，32（03），46-49
+%}
+clc;
+clear;
+T = 1e-3;
+s = tf('s');
+Gs = 0.00011167*(s + 33006)/(s*s + 1.245*s)
+[num,den] = tfdata(c2d(Gs,T,'tustin'),'v');
+figure(1)
+step(feedback(Gs,1))
